7f2f227ce20dda2290ac4600272d8fde6d2ccff9
geneaky <kaktus418@gmail.com>
2024-07-22 07:35:23 +0900

    section 1-6

---
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -0,0 +1,17 @@
+package tobyspring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class PaymentService {
+
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", new BigDecimal(50.7));
+        System.out.println(payment);
+    }
+}
